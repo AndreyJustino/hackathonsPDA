e9f0148105cfc9b1bf5474549f981c1fd1c7dedf
kennedfer <kenned14santos@gmail.com>
2024-03-23 13:46:00 -0300

    feat: implementação simples de database e remoção do utils, por enquanto

--- scripts/database/database.js
+++ scripts/database/database.js
@@ -1,5 +1,17 @@
 class DatabaseDenuncias {
   _denuncias = {};
 
-  cadastrarDenuncia(denuncia) {}
+  _obterId() {
+    return Date.now();
+  }
+
+  cadastrarDenuncia(denuncia) {
+    const denunciaId = this._obterId;
+
+    this._denuncias[denunciaId] = denuncia;
+  }
+
+  obterDenuncias() {
+    return this._denuncias;
+  }
 }
