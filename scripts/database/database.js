e4655e471a01788c81e77f05d40444a1d7f1f703
kennedfer <kenned14santos@gmail.com>
2024-03-23 14:12:21 -0300

    feat: Documentando o database

--- scripts/database/database.js
+++ scripts/database/database.js
@@ -1,16 +1,31 @@
 class DatabaseDenuncias {
   _denuncias = {};
 
+  /**
+   * Função para gerar ID
+   * @description Função que gera um ID único com base no timestamp atual e o retorna
+   * @returns {number} ID único baseado no timestamp
+   */
   _obterId() {
     return Date.now();
   }
 
+  /**
+   * Função para salvar denuncia
+   * @description Função salva um objeto "denúncia" no database
+   * @param {object} denuncia Denúncia que será salva
+   */
   cadastrarDenuncia(denuncia) {
     const denunciaId = this._obterId();
 
     this._denuncias[denunciaId] = denuncia;
   }
 
+  /**
+   * Função para obter denuncias
+   * @description Função que retorna todas as denúncias salvas
+   * @returns {object} Lista de denúncias
+   */
   obterDenuncias() {
     console.log(this._denuncias);
     return this._denuncias;
