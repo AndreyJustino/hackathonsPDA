92a66635b4e3ad2f994cfe8225e0a5d98cb891af
kennedfer <kenned14santos@gmail.com>
2024-03-23 14:21:35 -0300

    feat: cadastro.js documentado

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -1,23 +1,32 @@
+//Importanto o database
+//Use sempre essa instancia para manter a consistencia de dados
+//Padrao singleton
 import databaseDenuncias from "./database/database.js";
 
+/**
+ * Função de evento para cadastro de denúncia
+ * @description Função que captura as infos dos inputs e chama "cadastrarDenuncia" do database
+ * @param {Event} e Evento de click, é importante para que evitemos o refresh da pagina via submit
+ */
 function cadastrarDenuncia(e) {
-  e.preventDefault();
+  e.preventDefault(); //Evita que a página seja atualizada pelo 'submit'
 
+  //Objeto que salva as infos dos inputs para salvamento posterior no database
   const denuncia = {
     nome: nomeInput.value,
     email: emailInput.value,
     tipo: tipoInput.value,
   };
 
+  //Chamando o metodo do database para salvamento da denuncia
   databaseDenuncias.cadastrarDenuncia(denuncia);
-  databaseDenuncias.obterDenuncias();
 }
 
+//Obtendo inputs
 const nomeInput = document.getElementById("input-nome");
 const emailInput = document.getElementById("input-email");
 const tipoInput = document.getElementById("input-tipo");
 
+//Obtendo botao de "submit"
 const denunciarButton = document.getElementById("button-denunciar");
-denunciarButton.addEventListener("click", cadastrarDenuncia);
-
-console.log(denunciarButton);
+denunciarButton.addEventListener("click", cadastrarDenuncia); //Atribuindo o evento ao botao
